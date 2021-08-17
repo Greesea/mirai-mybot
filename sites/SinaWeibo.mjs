1ccddb96d0cb99ad50f37a9071ea7badd2d55962
Greesea <Greesea@vip.qq.com>
2021-08-17 15:53:19 +0800

    feature: add sinaweibo(tweet) support feature: add m.weibo.cn/detail/{id} support for sites/SinaWeibo.mjs

--- sites/SinaWeibo.mjs
+++ sites/SinaWeibo.mjs
@@ -7,7 +7,7 @@ import {requestUserAgentMobile} from "../settings.mjs";
 
 export default class SinaWeiboSite extends Site {
     name = "SinaWeibo";
-    regex = /(m\.weibo\.cn\/status\/.+)|(weibo\.com\/.+\/.+)/i;
+    regex = /(m\.weibo\.cn\/(status|detail)\/.+)|(weibo\.com\/.+\/.+)/i;
 
     //              $render_data = [..............][0] || {}
     contentRegex = /(?<=\$render_data\s?=\s?\[).+?(?=\]\[0\]\s?\|\|\s?\{\})/is;
@@ -43,18 +43,27 @@ export default class SinaWeiboSite extends Site {
             return new ReplyMessage(CreateMessageChain.plain(`[${this.name}] 获取微博信息失败：${id}`));
         }
 
-        //current only support video type
-        if (response.status?.page_info?.type !== "video") {
-            console.log(`[${this.name}] not supported type "${response.status?.page_info?.type}"`);
-            return;
-        }
-
         let replyMessage = new ReplyMessage();
+        let isVideo = response.status?.page_info?.type === "video";
+        let content, author, thumbnailUrl;
         let thumbnailRelativePath;
         let thumbnailAbsolutePath;
+
+        if (isVideo) {
+            if (response.status?.page_info?.page_pic?.url)
+                thumbnailUrl = response.status.page_info.page_pic.url;
+            content = response?.status?.page_info?.content2;
+            author = response?.status?.user?.screen_name;
+        } else {
+            if (response.status?.pics?.length)
+                thumbnailUrl = response.status.pics[0]?.url;
+            content = `${(response.status?.text ?? "").substring(0, 15)}${(response.status?.text ?? "").length > 15 ? "..." : ""}`;
+            author = response?.status?.user?.screen_name;
+        }
+
         try {
-            if (response.status?.page_info?.type === "video" && response.status?.page_info?.page_pic?.url)
-                ({thumbnailRelativePath, thumbnailAbsolutePath} = await thumbnailDownloader(response.status.page_info.page_pic.url));
+            if (thumbnailUrl)
+                ({thumbnailRelativePath, thumbnailAbsolutePath} = await thumbnailDownloader(thumbnailUrl));
         } catch (e) {
             console.warn(`[${this.name}] download ${id} thumbnail failed`);
         }
@@ -64,7 +73,7 @@ export default class SinaWeiboSite extends Site {
             replyMessage.pushTempFiles(thumbnailAbsolutePath);
         }
         replyMessage.pushMessage(
-            CreateMessageChain.plain(`${thumbnailRelativePath ? "\n" : ""}${response?.status?.page_info?.title ?? "[无标题]"}\nuser: ${response?.status?.user?.screen_name ?? "[无]"}\n${urlItem.raw}`)
+            CreateMessageChain.plain(`${thumbnailRelativePath ? "\n" : ""}${content ?? "[无内容]"}\nuser: ${author ?? "[无]"}\n${urlItem.raw}`)
         );
 
         return replyMessage;
